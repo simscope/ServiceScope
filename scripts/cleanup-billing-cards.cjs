8aee5978db93ae7c758d3c1978595dae6417f54b
Simscope <sandrvlad@gmail.com>
2026-06-26 20:11:46 -0400

    Clean duplicate billing card wrapper

--- scripts/cleanup-billing-cards.cjs
+++ scripts/cleanup-billing-cards.cjs
@@ -1,1 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const p = path.resolve(__dirname, '../src/components/OwnerPages.tsx');
+let c = fs.readFileSync(p, 'utf8');
+const a = ['        </div>', '        </div>'].join('');
+const b = ['        </div>'].join('');
+c = c.split(a).join(b);
+fs.writeFileSync(p, c);
 console.log('cleanup billing cards');
